2c68e9562f0413a0faa980b8b24125d07ff53d8a
SharonNJD <sharonnjd@gmail.com>
2023-11-16 10:54:43 +0200

    Add project files.

---
+++ CalculatorServiceSharon/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorServiceSharon
+{
+    public class Calculator : ICalculator
+    {
+        public double Div(double x, double Y)
+        {
+           return x/Y;
+        }
+
+        public double Mul(double x, double Y)
+        {
+           return x*Y;
+        }
+
+        public double Sub(double x, double Y)
+        {
+            return x-Y;
+        }
+
+        public double Sum(double x, double Y)
+        {
+           return x+Y;
+        }
+    }
+}
---
+++ CalculatorServiceSharon/ICalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorServiceSharon
+{
+    [ServiceContract]
+    public interface ICalculator
+    {
+        [OperationContract]
+        double Sum(double x,double Y);
+        [OperationContract]
+        double Sub(double x, double Y);
+        [OperationContract]
+        double Mul(double x, double Y);
+        [OperationContract]
+        double Div(double x, double Y);
+    }
+}
